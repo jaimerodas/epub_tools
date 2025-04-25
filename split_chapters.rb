f8a930600ce273322a8d77c231b54eab1ca68092
Jaime Rodas <jaime@rodas.mx>
2025-04-25 09:37:40 -0600

    SplitChapters becomes a class

--- split_chapters.rb
+++ split_chapters.rb
@@ -2,66 +2,79 @@ require 'nokogiri'
 require './text_style_class_finder'
 require './xhtml_cleaner'
 
-# === CONFIGURATION ===
-INPUT_FILE = ARGV[0] || abort("Usage: ruby split_chapters.rb input.xhtml")
-OUTPUT_PREFIX = 'chapter'
-OUTPUT_DIR = './chapters' # make sure this exists or create it
+class SplitChapters
+  def initialize(input_file, output_dir = './chapters', output_prefix = 'chapter')
+    @input_file    = input_file
+    @output_dir    = output_dir
+    @output_prefix = output_prefix
+  end
 
-# === SETUP ===
-Dir.mkdir(OUTPUT_DIR) unless Dir.exist?(OUTPUT_DIR)
+  def run
+    # Prepare output dir
+    Dir.mkdir(@output_dir) unless Dir.exist?(@output_dir)
 
-# Load and parse the XHTML file
-html = File.read(INPUT_FILE)
-doc = Nokogiri::HTML(html)
+    # Read the doc
+    doc = Nokogiri::HTML(File.read(@input_file))
 
-TextStyleClassFinder.new(INPUT_FILE).call
+    # Find Style Classes
+    TextStyleClassFinder.new(@input_file).call
 
-# Prepare variables
-current_chapter_number = nil
-chapter_docs = {}
+    chapters = extract_chapters(doc)
+    write_chapter_files(chapters)
+  end
 
-# Traverse all elements inside <body>
-body = doc.at('body')
-builder = Nokogiri::HTML::DocumentFragment.parse("")
-current_doc = nil
+  private
 
-body.children.each do |node|
-  if node.text =~ /Chapter\s+(\d+)/i && %w[p span h3].include?(node.name)
-    # Save current chapter if one is open
-    if current_chapter_number
-      chapter_docs[current_chapter_number] = current_doc.to_html
+  def extract_chapters(doc)
+    chapters = {}
+    current_number = nil
+    current_fragment = nil
+
+    doc.at('body').children.each do |node|
+      if node.text =~ /Chapter\s+(\d+)/i && %w[p span h3].include?(node.name)
+        chapters[current_number] = current_fragment.to_html if current_number
+        current_number = Regexp.last_match(1).to_i
+        current_fragment = Nokogiri::HTML::DocumentFragment.parse('')
+        current_fragment.add_child(node.dup)
+      else
+        current_fragment&.add_child(node.dup)
+      end
     end
 
-    # Start new chapter
-    current_chapter_number = $1.to_i
-    current_doc = Nokogiri::HTML::DocumentFragment.parse("")
-    current_doc.add_child(node.dup)
-  else
-    current_doc&.add_child(node.dup)
+    chapters[current_number] = current_fragment.to_html if current_number
+    chapters
   end
+
+  def write_chapter_files(chapters)
+    chapters.each do |number, content|
+      write_chapter_file(number, content)
+    end
+  end
+
+  def write_chapter_file(number, content)
+    filename = File.join(@output_dir, "#{@output_prefix}_#{number}.xhtml")
+    File.write(filename, <<~HTML)
+      <?xml version="1.0" encoding="UTF-8"?>
+      <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+        <head>
+          <title>DM and the Dirty 20s - Chapter #{number}</title>
+          <link rel="stylesheet" type="text/css" href="style.css"/>
+        </head>
+        <body>
+          <h1>Chapter #{number}</h1>
+          #{content}
+        </body>
+      </html>
+    HTML
+    puts "Wrote #{filename}"
+    XHTMLCleaner.new(filename).call
+  end
+
 end
 
-# Save the last chapter
-if current_chapter_number
-  chapter_docs[current_chapter_number] = current_doc.to_html
-end
-
-# Write to files
-chapter_docs.each do |number, content|
-  filename = File.join(OUTPUT_DIR, "#{OUTPUT_PREFIX}_#{number}.xhtml")
-  File.write(filename, <<~HTML)
-    <?xml version="1.0" encoding="UTF-8"?>
-    <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-      <head>
-        <title>DM and the Dirty 20s - Chapter #{number}</title>
-        <link rel="stylesheet" type="text/css" href="style.css"/>
-      </head>
-      <body>
-        <h1>Chapter #{number}</h1>
-        #{content}
-      </body>
-    </html>
-  HTML
-  puts "Wrote #{filename}"
-  XHTMLCleaner.new(filename).call
+if __FILE__ == $0
+  input_file    = ARGV[0] || abort("Usage: ruby #{__FILE__} input.xhtml [output_dir] [prefix]")
+  output_dir    = ARGV[1] || './chapters'
+  output_prefix = ARGV[2] || 'chapter'
+  SplitChapters.new(input_file, output_dir, output_prefix).run
 end
